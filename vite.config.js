f58cb54610c51574be0ada929facec1e19f893b3
Zazzouz <zakazzouz00@gmail.com>
2025-04-21 22:18:14 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  base: '/personal-portfolio/', // this must match your repo name
-  plugins: [react(), ghPages()],
+  base: '/personal-portfolio/', // Must match your GitHub repo name
+  plugins: [react()],
 });
