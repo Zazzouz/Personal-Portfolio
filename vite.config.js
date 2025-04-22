f1ee77eb7fc5d17c183b599a56bf0d7468004d14
Zazzouz <zakazzouz00@gmail.com>
2025-04-21 22:30:49 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/personal-portfolio/', // Must match your GitHub repo name
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
